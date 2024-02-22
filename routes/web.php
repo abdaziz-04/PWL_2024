9c860a153271b5415088b1347e29440cb0182128
Abdul Aziz <52687444+Aziz2123@users.noreply.github.com>
2024-02-22 14:49:17 +0700

    praktikum3 nyoba 1

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,7 @@ Route::get('posts/{post}/comments/{comment}', function ($postId, $commentId) {
 Route::get('article/{id}', function ($id) {
     return 'Halaman artikel dengan id ' . $id;
 });
+
+Route::get('/user/{name?}', function ($name = null) {
+    return 'Nama saya ' . $name;
+});
